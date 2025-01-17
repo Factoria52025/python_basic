640a538a4530c9a8b0bcf7b1eb61e2b8da302cbd
Factoria52025 <bcalvoarellano@gmail.com>
2025-01-17 14:37:52 +0100

    iniciar subida del proyecto

---
+++ operators.py
@@ -0,0 +1,145 @@
+"""
+--------------------------- COLECCIONES ---------------------------
+En este taller aprenderás a manipular coleccciones de datos: Listas, diccionarios, tuplas y sets.
+"""
+"""
+ --- LISTAS ---
+Las listas son ordenadas y mutables.
+Pueden contener elementos duplicados.
+Puedes modificar, añadir y eliminar elementos.
+"""
+"""
+--- Ejercicio 1 Listas ---
+Crea una variable "mascotas" que almacene una lista con los siguientes elementos: 'perro', 'gato', 'loro'
+Imprime por consola el valor almacenado
+Despues haz los pasos pedidos
+"""
+# mascotas = ['perro', 'gato', 'loro']
+# print(mascotas)
+# # Escribe el código para saber la cantidad de elementos que tiene la lista, imprimir por consola
+# print("Cantidad de elementos de la lista,", len(mascotas))
+# # Escribe el código para acceder al valor de la posición 2, imprimir por consola
+# print("Posición 2 de mi lista,", mascotas[1])
+# # Escribe el código para agregar un elemento a la lista, imprimir por consola la lista
+# mascotas.append('pez')
+# print("Añadir nuevo elemento a mi lista,", mascotas)
+# # Escribe el código para modificar un elemento de la lista, imprimir por consola la lista
+# mascotas[0] = 'caballo'
+# print("Elemento modificado de mi lista,", mascotas )
+# # Escribe el código para eliminar un elemento de la lista, imprimir por consola la lista
+# # mascotas.remove('gato')
+# del mascotas[1]
+# print("Elemento de la lista eliminado", mascotas)
+"""
+ --- TUPLAS ---
+Las tuplas son ordenadas e inmutables.
+Pueden contener elementos duplicados.
+No puedes modificar, añadir o eliminar elementos después de la creación.
+"""
+"""
+--- Ejercicio 2 Tuplas ---
+Crea una variable "plantas" que almacene una tupla con los siguientes elementos: 'cactus', 'orquidea', 'rosas'
+Imprime por consola el valor almacenado
+Despues haz los pasos pedidos
+"""
+plantas = ('cactus', 'orquidea', 'rosas')
+print(plantas)
+# Escribe el código para saber la cantidad de elementos que tiene la tupla, imprimir por consola
+print("Número de elementos en nuestra tupla,", len(plantas))
+# Escribe el código para acceder al valor de la posición 2, imprimir por consola
+print("Valor de la posición 2,", plantas[1])
+# Intentar modificar una tupla
+# plantas[1] = 'hoja rota'  # Descomenta esta línea para ver qué sucede
+# Escribe tu análisís acá acerca de qué sucede
+#Las tuplas son inmutables, no podemos modificar, añadir o eliminar ningún elemento de las mismas.
+"""
+ --- SETS ---
+Los sets son desordenados y mutables.
+No pueden contener elementos duplicados.
+Puedes añadir y eliminar elementos, pero no puedes modificar los elementos existentes.
+"""
+"""
+--- Ejercicio 3 Sets ---
+Crea una variable "nombres" que almacene un set con los siguientes elementos: 'María', 'Cris', 'Cris', 'Alex'
+Imprime por la terminal dicha variable
+Haz los pasos pedidos
+"""
+# Escribe el código aqui
+# Explica qué sucede cuándo imprimes el valor que almacena "nombres"
+# Escribe el código para saber la cantidad de elementos que tiene el set, imprimir por consola
+# Escribe el código para acceder al valor de la posición 3, imprimir por consola
+# Escribe el código para agregar una elemento al set, imprimir por consola el set
+# Escribe el código para eliminar un elemento del set, imprimir por consola el set
+"""
+ --- DICCIONARIOS ---
+Los diccionarios son desordenados y mutables.
+Contienen pares clave-valor.
+Puedes añadir, modificar y eliminar pares clave-valor.
+"""
+"""
+--- Ejercicio 4 Diccionarios ---
+Crea un diccionario llamado "ciudad" con las claves 'nombre' y 'pais' y los valores 'Barcelona' y 'España' respectivamente.
+Imprime el diccionario
+"""
+# Escribe el código aqui para acceder y ver por consola el valor de 'nombre'
+# Escribe el código aqui para añadir un nuevo par clave-valor y ver por consola el valor de 'ciudad'
+# Escribe el código aqui para modificar el valor de un par clave-valor de 'ciudad' y verlo por consola
+# Escribe el código aqui para eliminar un par clave-valor de 'ciudad' y verlo por consola
+13:06
+--------------------------------
+13:06
+"""
+--------------------------- OPERADORES ---------------------------
+En este taller aprenderás usar los diferentes tipos de operadores
+"""
+"""
+--- Ejercicio 1 operadores aritméticos  ---
+Reemplaza el ? con el operador según sea el caso
+"""
+num1 = 10
+num2 = 5
+# print(f"Suma: {num1 ? num2}")
+# print(f"Resta: {num1 ? num2}")
+# print(f"Multiplicación: {num1 ? num2}")
+# print(f"División: {num1 ? num2}")
+"""
+--- Ejercicio 2 operadores lógicos  ---
+Reemplaza el ? con el operador según sea el caso
+Puedes investigar sobre las tablas de la verdad, recuerda "las amigas de mis amigas, son mis amigas"
+"""
+michi_4_patas = True
+michi_3_ojos = False
+michi_vuela = False
+michi_interesado = True
+michi_juega = True
+michi_pone_huevos = False
+# print(f"AND: {michi_4_patas ? michi_3_ojos}")
+# print(f"OR: {michi_interesado ? michi_vuela}")
+# print(f"NOT bool1: {? michi_juega}")
+# print(f"NOT bool2: {? michi_pone_huevos}")
+"""
+--- Ejercicio 3 Comparadores  ---
+Reemplaza el ? con el comparador según sea el caso
+"""
+a = 7
+b = 3
+# print(f"¿a es igual a b?: {a ? b}")
+# print(f"¿a es diferente de b?: {a ? b}")
+# print(f"¿a es mayor que b?: {a ? b}")
+# print(f"¿a es menor que b?: {a ? b}")
+# print(f"¿a es mayor o igual que b?: {a ? b}")
+# print(f"¿a es menor o igual que b?: {a ? b}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
